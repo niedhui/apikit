4f0fd20bd4320f2f8810c8f1c4d390f7246e64e9
niedhui <dianhui.nie@gmail.com>
2015-06-30 17:41:04 +0800

    统一签名

--- lib/apikit/client.rb
+++ lib/apikit/client.rb
@@ -15,26 +15,30 @@ module Apikit
     #   query:
     # @return [Sawyer::Resource]
     def get(path, options = {})
-      request :get, path, nil, options
+      request :get, path, options
     end
 
     # @return [Sawyer::Resource]
-    def post(path, data, options = {})
-      request :post, path, data, options
+    def post(path, options = {})
+      request :post, path, options
     end
 
     # @return [Sawyer::Resource]
-    def put(path, data, options = {})
-      request :put, path, data, options
+    def put(path, options = {})
+      request :put, path, options
     end
 
     # @return [Sawyer::Resource]
     def delete(path, options = {})
-      request :delete, path, nil, options
+      request :delete, path, options
     end
 
     # @return [Sawyer::Resource]
-    def request(method, path, data, options = {})
+    # options:
+    #   query: ?a=b&c=d
+    #   data: params in body
+    def request(method, path, options = {})
+      data = options.delete(:data)
       @last_response = response = agent.call(method, URI::Parser.new.escape(path.to_s), data, options)
       response.data.tap do |data|
         broadcast :request_fail, response.status, data, response unless [200, 201].include?(response.status)
